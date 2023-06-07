1d5de9297fce7a52a1ebe36bd5e24365f877bee1
ihorkoi <koleev.igor@gmail.com>
2023-06-07 15:53:54 +0300

    solved

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
+galleryRef.addEventListener('click', onImageClick);
 
-console.log(galleryItems);
+generateGalleryMarckup(galleryItems);
+
+function generateGalleryMarckup(imageOdjectArray) {
+    let galleryMarckup = '';
+    for (const imageOdject of imageOdjectArray) {
+        const { original, preview, description } = imageOdject;
+        galleryMarckup += `<li class="gallery__item"><a class="gallery__link" href="${original}"><img  class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`;
+    galleryRef.innerHTML = galleryMarckup;
+   
+    }
+}
+function onImageClick(evt) {
+    evt.preventDefault();
+
+    if (!evt.target.classList.contains('gallery__image')) {
+        return
+    }
+
+    const instance = basicLightbox.create(`
+        <div class="modal">
+            <img src="${evt.target.dataset.source}">
+        </div>`)
+    
+    instance.show()
+
+    document.addEventListener("keydown", event => {
+        if (event.key ==='Escape'){
+            instance.close();
+        }
+    });
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.addEventListener('click', onImageClick);
+
+generateGalleryMarckup(galleryItems);
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: '250',captionClass:'gallery__caption'});
+
+function generateGalleryMarckup(imageOdjectArray) {
+    let galleryMarckup = '';
+    for (const imageOdject of imageOdjectArray) {
+        const { original, preview, description } = imageOdject;
+        galleryMarckup += `<li class="gallery__item"><a class="gallery__link" href="${original}"><img  class="gallery__image" src="${preview}" alt="${description}"/></a></li>`;
+    galleryRef.innerHTML = galleryMarckup;
+   
+    }
+}
 
 console.log(galleryItems);
+
+function onImageClick(evt) {
+    evt.preventDefault();
+}
